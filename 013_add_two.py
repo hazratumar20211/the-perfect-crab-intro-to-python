62bd6b3610252a7e853a4c257895203fe2a32968
hazratumar20211 <jan.jan20211@gmail.com>
2025-10-25 11:07:16 -0700

    Update 013_add_two.py

--- 013_add_two.py
+++ 013_add_two.py
@@ -11,7 +11,11 @@ from lib.helpers import check_that_these_are_equal
 # * Returns the result
 
 # YOUR FUNCTION GOES BELOW THIS LINE
+def add_two(number):
+  number += 2
+  return number
 
+number = int(input("Write your number here \n"))
 
 
 # YOUR FUNCTION GOES ABOVE THIS LINE
