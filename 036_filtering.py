4689bcd5974b75326fa3aaf8dc9ed34bed95482e
hazratumar20211 <jan.jan20211@gmail.com>
2025-10-27 14:19:03 -0700

    Add function to return only positive numbers
    Implement function to filter positive numbers from a list.

--- 036_filtering.py
+++ 036_filtering.py
@@ -36,6 +36,11 @@ print("Function: only_positive_numbers")
 
 # Return a new list with only the positive numbers
 def only_positive_numbers(numbers):
+  positive_numbers = []
+  for number in numbers:
+    if number > 0:
+      positive_numbers.append(number)
+  return positive_numbers
   pass
 
 check_that_these_are_equal(
